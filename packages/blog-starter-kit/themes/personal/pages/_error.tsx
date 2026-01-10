fa20073e6c9baa121150f21b5f7b112f1c9d4146
Koen van Hove <koen@koenvh.nl>
2026-01-10 19:43:28 +0100

    Fix error page

--- packages/blog-starter-kit/themes/personal/pages/_error.tsx
+++ packages/blog-starter-kit/themes/personal/pages/_error.tsx
@@ -1,20 +1,16 @@
 import { GetServerSideProps } from "next";
+import Link from "next/link";
 
-export default function About() {
-  return <div>Whoops, that page does not seem to exist. <a href="/">Go back home</a>.</div>
+export default function Error() {
+  return <div>Whoops, that page does not seem to exist. <Link href="/">Go back home</Link>.</div>
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const { req, res, query } = ctx;
 
-    const host = process.env.NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST;
-	if (!host) {
-		throw new Error('Could not determine host');
-	}
-
     if (req.method && req.method == "POST") {
         res.statusCode = 303;
-        res.setHeader("Location", req.url ?? host);
+        res.setHeader("Location", req.url ?? "/");
         res.end();
     }
     return { props: {} };
