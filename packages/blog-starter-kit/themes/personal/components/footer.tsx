22b95460efff6170a93da4b00a7c6c7082bde52a
Koen van Hove <koen@koenvh.nl>
2026-01-18 15:12:27 +0100

    Add RSS feed and sitemap to footer

--- packages/blog-starter-kit/themes/personal/components/footer.tsx
+++ packages/blog-starter-kit/themes/personal/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useAppContext } from './contexts/appContext';
 
 export const Footer = () => {
@@ -5,7 +6,7 @@ export const Footer = () => {
 
 	return (
 		<footer className="border-t pt-10 text-sm text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">
-			&copy; {new Date().getFullYear()} {publication.title}
+			&copy; {new Date().getFullYear()} {publication.title} | <a href="/rss.xml">RSS feed</a> | <a href="/sitemap.xml">Sitemap</a>
 		</footer>
 	);
 };
