e5a9084138990ed79997be41193d843df970b16c
Koen van Hove <koen@koenvh.nl>
2026-02-01 16:10:27 +0100

    Use medium font on index

---
+++ packages/blog-starter-kit/themes/personal/start.bat
@@ -0,0 +1,5 @@
+set NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT=https://gql.hashnode.com
+set NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST=koen.hashnode.dev
+set NEXT_PUBLIC_MODE=production
+set NEXT_PUBLICATION_URL=https://blog.koenvh.nl
+npx pnpm@8.15.9 dev
